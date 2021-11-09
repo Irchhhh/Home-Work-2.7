421b2f2e47e7644b690172ae0e847ddd211d7e73
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2021-11-09 16:51:58 +0300

    Initial Commit

---
+++ Home Work 2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 2.7
+//
+//  Created by Ирина on 09.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
